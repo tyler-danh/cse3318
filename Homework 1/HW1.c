c3d583172f01c320dd798f2d61fd4dac4f88b1ac
Tyler Danh <txd6321@mavs.uta.edu>
2023-06-14 16:45:01 -0500

    homework code

---
+++ Homework 1/HW1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    #ifdef Woof
+        int Dog[] = {2,3,4,10,42,47,49,50};
+    #else
+        int Dog[] = {2,3,4,10,11,15,40,42,47,49};
+    #endif
+
+    int i = 0;
+    int sum = 0;
+    int Cat = sizeof(Dog)/sizeof(Dog[0]);
+
+    for(i = 0; i<Cat; i++)
+        sum+=Dog[i];
+
+    printf("%d", sum);
+   
+    return 0;
+}

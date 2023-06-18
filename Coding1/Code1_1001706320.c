482259a8d2f50e7028ce198e45d429c88835a205
Tyler Danh <txd6321@mavs.uta.edu>
2023-06-18 00:19:15 -0500

    PROG

--- Coding1/Code1_1001706320.c
+++ Coding1/Code1_1001706320.c
@@ -13,37 +13,99 @@ NODE;
 
 void AddNodeToLL(int Number, NODE **LinkedListHead)
 {
+	NODE *newNode = (NODE*)malloc(sizeof(NODE));
+	newNode->number = Number;
+	newNode->next_ptr = NULL;
+	NODE *temp = *LinkedListHead;
+
+	if(*LinkedListHead = NULL)
+		*LinkedListHead = newNode;
+	else
+	{
+		while(temp->next_ptr != NULL)
+			temp = temp->next_ptr;
+		temp = newNode;
+	}
 }
 
 void ReadFileIntoLL(int argc,  char *argv[], NODE **LLH)
 {
+	FILE* io = fopen(argv[1], "r");
+	if(io == NULL)
+	{
+		printf("File Failed to Open Exiting...");
+		exit;
+	}
+	int num = 0;
+
+	while(fgets(num, sizeof(num), io) != NULL)//figure out what to use for integer because fgets is for chars
+	{
+		AddNodeToLL(num, LLH);
+	}
 }
 
 void PrintLL(NODE *LLH) 
-{ 
+{
+	NODE *temp = LLH;
+	while(temp->next_ptr != NULL)
+	{
+		printf("%p, %d, %p", temp, temp->number, temp->next_ptr);
+		temp = temp->next_ptr;
+	}
 }
 
 void FreeLL(NODE **LLH) 
 { 
+	NODE *temp = *LLH;
+	NODE *nextNode;
+
+	while(temp->next_ptr != NULL)
+	{
+		nextNode = temp->next_ptr;
+		free(temp);
+		temp = nextNode;
+		#ifdef PRINT
+			printf("Freeing %p %d %p", temp, temp->number, temp->next_ptr);
+		#endif
+	}
 }
 
-int main(/* add argc and argv */) 
+int main(int argc, char *argv[]) 
 { 
+	
+
 	NODE *LLH = NULL;
+	int start = 0;
+	int end = 0;
 	
 	/* capture the clock in a start time */
+	start = clock();
+
 	ReadFileIntoLL(argc, argv, &LLH);
+
 	/* capture the clock in an end time */
+	end = clock();
 	printf("\n%ld tics to write the file into the linked list\n", end-start);
 
-	/* capture the clock in a start time */
-	PrintLL(LLH);
-	/* capture the clock in an end time */
-	printf("\n%ld tics to print the linked list\n", end-start);
+	#ifdef PRINT
+		/* capture the clock in a start time */
+		start = clock();
+
+		PrintLL(LLH);
+
+		/* capture the clock in an end time */
+		end = clock();
 	
+		printf("\n%ld tics to print the linked list\n", end-start);
+	#endif
+
 	/* capture the clock in a start time */
+	start = clock();
+
 	FreeLL(&LLH);
+
 	/* capture the clock in an end time */
+	end = clock();
 	printf("\n%ld tics to free the linked list\n", end-start);
 	
 	return 0; 

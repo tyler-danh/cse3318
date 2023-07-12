61786eec5f1ae3a381bcc3516ae31167d0dc6752
Tyler Danh <txd6321@mavs.uta.edu>
2023-07-12 14:25:11 -0500

    HW1

--- Homework/HW1.c
+++ Homework/HW1.c
@@ -5,6 +5,8 @@ int main(void)
 {
     #ifdef Woof
         int Dog[] = {2,3,4,10,42,47,49,50};
+    #elif Bark
+        int Dog[] = {1,2,3,4,5};
     #else
         int Dog[] = {2,3,4,10,11,15,40,42,47,49};
     #endif

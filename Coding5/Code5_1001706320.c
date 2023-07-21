f2b36e9ba436da0030b427b69ba68b6839f4e1e9
Tyler Danh <txd6321@mavs.uta.edu>
2023-07-21 15:48:41 -0500

    prog

--- Coding5/Code5_1001706320.c
+++ Coding5/Code5_1001706320.c
@@ -2,7 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 
+int ReadFileIntoArray(int argc, char *argv[], int **Array)
+{
+    FILE* io = fopen(argv[1], "r");
+	if(io == NULL)
+	{
+		printf("File must be provided on command line... exiting\n");
+		exit(1);
+	}
+}
+
 int main(int argc, char *argv[])
 {
-
+	
 }

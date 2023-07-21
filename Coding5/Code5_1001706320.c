721d9f146361686f1a92eec0b42a728bd41602b6
Tyler Danh <txd6321@mavs.uta.edu>
2023-07-21 15:59:04 -0500

    progress

--- Coding5/Code5_1001706320.c
+++ Coding5/Code5_1001706320.c
@@ -1,18 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define MAX 5
 
-int ReadFileIntoArray(int argc, char *argv[], int **Array)
+void CreateAdjacencyMatrix(int AdjMatrix[][MAX])
 {
-    FILE* io = fopen(argv[1], "r");
-	if(io == NULL)
+	int start = 0, end = 0;
+	int i = 0, j = 0;
+	char buffer[100] = {};
+	FILE *FH = fopen("EdgeList.txt", "r+");
+	if (FH == NULL)
+		exit(0);
+	/* initialize adjacency matrix to -1 */
+	for(i = 0; i < MAX; i++)
+		for(j = 0; j < MAX; j++)
+			AdjMatrix[i][j] = -1;
+	while (fgets(buffer, sizeof(buffer)-1, FH))
 	{
-		printf("File must be provided on command line... exiting\n");
-		exit(1);
+		sscanf(buffer, "%d %d", &start, &end); //this will need fixing when slides come out
+		AdjMatrix[start][end] = 1;
+		//figure out how to read the entire line, these are for ints, we will be using strtok()
 	}
+	fclose(FH);
 }
 
 int main(int argc, char *argv[])
 {
-	
+	int AdjMatrix[MAX][MAX]; //maybe this wont be an array of int?
 }
